295451a0ed99a2c0522bd7612b46bcbf2e8e84aa
RoktimTaye <roktimtayecoding@gmail.in>
2025-07-30 10:30:37 +0530

    updated

---
+++ MyAnimationUnderstanding/MyEventListning/MyEvent.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector("h1");
+window.addEventListener("keydown", (event) => {
+    if(event.key === " ") {
+        h1.textContent = "SPC";
+    }
+    else{
+        h1.textContent = event.key;
+    }
+}); // This code listens for keydown events and updates the h1 text content based on the pressed key. If the spacebar is pressed, it displays "SPC"; otherwise, it shows the name of the key pressed.
