eff6d3d5ec62481ddb3b1be39672ef4636bbdda8
RoktimTaye <roktimtayecoding@gmail.in>
2025-08-03 15:47:05 +0530

    August 3 decond commit

--- MyJs/MyEventListning/CharCount.js
+++ MyJs/MyEventListning/CharCount.js
@@ -2,7 +2,9 @@ let inp= document.querySelector("input");
 let span = document.querySelector("span");
 
 inp.addEventListener("input", function(){
+     // Number logic to calculate remaining characters
     let left = 20 - inp.value.length;
+    // Displaying the remaining characters in the span
     if(left < 0){
         span.textContent = left
         span.style.color = "red";
