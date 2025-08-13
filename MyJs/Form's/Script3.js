c0473ea53fcce7d015f198bf7c1b2f3e451bac68
RoktimTaye <roktimtayecoding@gmail.in>
2025-08-13 16:26:41 +0530

    Auguat 13 firat commit

--- MyJs/Form's/Script3.js
+++ MyJs/Form's/Script3.js
@@ -2,7 +2,7 @@ let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let form = document.querySelector("#loginForm");
 
-form.addEventListener("submit",function(dets){
+form.addEventListener("submit", function(dets){
     dets.preventDefault();
 
     document.querySelector("#emailError").textContent = "";
@@ -12,24 +12,25 @@ form.addEventListener("submit",function(dets){
     const passwordRegex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
 
     let emailAns = emailRegex.test(email.value);
-    let passwordAns = passwordRegex.test(password.value);
+    let passwordAns = passwordRegex.test(email.value);
 
     let isValid = true;
 
-    if(!emailAns) {
+    if(!emailAns){
         document.querySelector("#emailError").textContent = "Email is incorrect";
         document.querySelector("#emailError").style.display = "initial";
         isValid = false;
     }
 
-    if (!passwordAns) {
+    if(!passwordAns){
         document.querySelector("#passwordError").textContent = "Password is incorrect";
         document.querySelector("#passwordError").style.display = "initial";
         isValid = false;
     }
 
     if(isValid){
-        document.querySelector("#resultMessage").textContent = "EveryThing is correct";
+        document.querySelector("#resultMessage").textContent = "Everything is fine";
+        
     }
 
-});
+})
