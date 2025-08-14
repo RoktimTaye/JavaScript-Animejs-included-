5d23f537d5476076e4789be4dd787cf84c500789
RoktimTaye <roktimtayecoding@gmail.in>
2025-08-14 15:09:13 +0530

    Aug 14 first commit

--- MyJs/Form's/Script3.js
+++ MyJs/Form's/Script3.js
@@ -1,32 +1,35 @@
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let form = document.querySelector("#loginForm");
-form.addEventListener("submit",function(dets){
+
+form.addEventListener("submit", function(dets){
     dets.preventDefault();
 
     document.querySelector("#emailError").textContent = "";
-    document.querySelector("#passwordError").textContent = "";
-    
-    const emailRegex = /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
+    document.querySelector("#epasswordError").textContent = "";
+
+    const emailRegex =  /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
 
     let emailAns = emailRegex.test(email.value);
     let passwordAns = passwordRegex.test(password.value);
 
     let isValid = true;
 
-    if(!emailAns){
+    if(!emailAns) {
         document.querySelector("#emailError").textContent = "Email is incorrect";
-        document.querySelector("#emailError").style.display = "initial";
+        Document.querySelector("#mailError").style.display="initial";
         isValid = false;
     }
+
     if(!passwordAns){
         document.querySelector("#passwordError").textContent = "Password is incorrect";
-        document.querySelector("#passwordError").style.display = "initial";
+        document.querySelector("#passowrdError").style.display = "initial";
         isValid = false;
     }
-    if(isValid){
-        document.querySelector("#resultMessage").textContent = "Login successful!";
+
+    if(isValid) {
+        document.querySelector("#resultMessage").textContent = "Everything is fine";
         document.querySelector("#resultMessage").style.display = "initial";
     }
-})
+});
